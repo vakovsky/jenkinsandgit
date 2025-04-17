253ccd694f279fb26b42152771bddadb0cd68644
Dimiter Vakovsky <vakovsky@gmail.com>
2025-04-17 22:18:37 +0300

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 123456");
+            Console.WriteLine("Hello Demo");
+
+        }
+    }
+}
