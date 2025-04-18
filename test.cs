3327342c5eef00b67d35a0cc3f8ab8edd65d8b8d
Dimiter Vakovsky <vakovsky@gmail.com>
2025-04-18 07:55:49 +0300

    Update test.cs

--- test.cs
+++ test.cs
@@ -10,9 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello 123456");
-            Console.WriteLine("Hello Demo");
-
+            Console.WriteLine("Hello 123");
         }
     }
 }
